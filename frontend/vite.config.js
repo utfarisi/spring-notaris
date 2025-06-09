65a076178a7c58b532c99ef18fc8627a1ba05e4b
sutarman <sutarman@gmail.com>
2025-06-09 11:42:53 +0700

    mengarahkan ke railway

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://spring-notaris-production.up.railway.app',
         changeOrigin: true,
         secure:false,
         configure: (proxy) => {
