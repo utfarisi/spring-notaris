18df23311242bfe9acef9ad90606b89ce9faacae
sutarman <sutarman@gmail.com>
2025-06-09 09:44:45 +0700

    update cors

--- backend/src/main/java/edu/ut/kelompokb/notaryapp/configs/WebConfig.java
+++ backend/src/main/java/edu/ut/kelompokb/notaryapp/configs/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173")
+                        .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173", "https://spring-notaris-production.up.railway.app")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true)
--- backend/src/main/java/edu/ut/kelompokb/notaryapp/controllers/FrontendController.java
+++ backend/src/main/java/edu/ut/kelompokb/notaryapp/controllers/FrontendController.java
@@ -6,14 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FrontendController {
 
-    // Ini akan mengarahkan semua path yang tidak cocok dengan REST API
-    // atau aset statis lainnya ke index.html Anda
     @GetMapping({
         "/",
-        "/about",
-        "/contact",
-        "/dashboard/**", // Contoh untuk rute yang dihandle oleh Vue Router
-        "/{path:[^\\.]*}" // Menangkap semua path yang tidak memiliki ekstensi file
+        "/{path:[^\\.]*}"
     })
     public String forwardToIndex() {
         return "forward:/index.html";
